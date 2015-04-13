ad374005769ea44fc67d4921543f6e80ed96d95c
Vyacheslav Tverskoy <tsx@tsx.su>
2015-04-13 10:31:11 +0500

    Less wavy renders

--- make-screenshot/make-screenshot.sh
+++ make-screenshot/make-screenshot.sh
@@ -7,5 +7,5 @@ fi
 echo "Rendering page..."
 phantomjs --disk-cache=true --local-to-remote-url-access=true --web-security=false "`dirname $0`"/make-screenshot.js $1 /tmp/r.png
 echo "Applying distortion filter..."
-convert /tmp/r.png \( +clone -channel G +noise Random -virtual-pixel Tile -blur 0x15 -auto-level -separate +channel \) -swap 0,1 -resize 70% miff:- | composite - -displace 4 /tmp/r.jpg
+convert /tmp/r.png \( +clone -channel G +noise Random -virtual-pixel Tile -blur 0x50 -auto-level -separate +channel \) -swap 0,1 -resize 70% miff:- | composite - -displace 5 /tmp/r.jpg
 mv /tmp/r.jpg "$2"
